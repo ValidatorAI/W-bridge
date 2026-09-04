28534950fd270086dff191187254e702ad1167a5
siavashmohammadi <siavash.mohammadi@flexiana.com>
2026-09-04 16:34:01 +0330

    removing un used code

--- application/query.py
+++ application/query.py
@@ -1,7 +1,3 @@
 from sqlalchemy.orm import Session
 
 from db.models import Bot
-
-
-def _get_active_bot_by_token_query(db: Session, token: str) -> Bot | None:
-	return db.query(Bot).filter(Bot.token == token, Bot.active.is_(True)).first()
